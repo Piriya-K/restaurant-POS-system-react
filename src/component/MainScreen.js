1fa9f5079d04c293feda44b8ef82a38298bc4448
Piriya-K <piriya619@gmail.com>
2024-01-10 15:52:36 -0700

    OrderPage (formerly Menu Page), backend func. done 1)display menu category, 2) display menu item after clicking on menu category, 3) clicking on menu item adds the item to a list and the list displays on screen, 4) able to adjust menu item quantity, 5) item disappears when quantity is 0, 6) The total value of the menu item list displays on screen

--- src/component/MainScreen.js
+++ src/component/MainScreen.js
@@ -7,7 +7,15 @@ const MainScreen = () => {
   const [itemlist, setItemList] = useState([]);
 
   const handleItemClick = (item) => {
-    setItemList([...itemlist, item]);
+    setItemList([
+      ...itemlist,
+      {
+        name: item.name,
+        price: item.price,
+        quantity: 1,
+        value: item.price,
+      },
+    ]);
   };
 
   const [selectedMenu, setSelectedMenu] = useState(null);
@@ -16,11 +24,26 @@ const MainScreen = () => {
     setSelectedMenu(menuType);
   };
 
+  const handleQuantityClick = (index, newQuantity) => {
+    const updatedItemList = [...itemlist];
+    if (index >= 0 && index < updatedItemList.length && newQuantity >= 1) {
+      updatedItemList[index].quantity = newQuantity;
+      updatedItemList[index].value = newQuantity * updatedItemList[index].price;
+      setItemList(updatedItemList);
+    } else {
+      setItemList((itemList) =>
+        index >= 0 && index < itemList.length
+          ? itemlist.filter((_, i) => i !== index)
+          : itemList
+      );
+    }
+  };
+
   return (
     <div className="grid grid-cols-3 flex-1 gap-8 bg-white p-5 sm:text-xs">
       <LeftPanel onCategoryClick={handleMenuClick} />
       <CenterPanel selectedMenu={selectedMenu} onItemClick={handleItemClick} />
-      <RightPanel itemlist={itemlist} />
+      <RightPanel itemlist={itemlist} onQuantityClick={handleQuantityClick} />
     </div>
   );
 };
--- src/component/RightPanel.js
+++ src/component/RightPanel.js
@@ -1,28 +1,65 @@
 import React from "react";
-import RightPanelItemList from "./RightPanelItemList";
 import { useState } from "react";
 
-const RightPanel = ({ itemlist }) => {
+const RightPanel = ({ itemlist, onQuantityClick }) => {
   const [totalValue, setTotalValue] = useState(0);
 
-  const receiveValueCallback = (valueDataFromChildComponent) => {
-    setTotalValue(valueDataFromChildComponent);
+  const setQuantity = (index, newQuantity) => {
+    onQuantityClick(index, newQuantity);
   };
 
+  const removeItemFromList = (index) => {
+    onQuantityClick(index);
+  };
+
+  const sumValue = itemlist.reduce((value, item) => {
+    return value + item.value;
+  }, 0);
+
   return (
-    <div className="text-red-500 shadow-lg rounded-md hover:bg-gray-100">
-      <div>
-        <ul>
-          {itemlist.map((item, index) => (
-            <RightPanelItemList
-              item={item}
-              key={index}
-              itemValueCallBack={receiveValueCallback}
-            />
-          ))}
-        </ul>
-      </div>
-      <div>${totalValue}</div>
+    <div className="shadow-lg rounded-md hover:bg-gray-100">
+      <div className="text-right mr-6">Total: ${sumValue.toFixed(2)}</div>
+      <ul>
+        {itemlist.map((item, index) => (
+          <div>
+            {item.quantity >= 1 ? (
+              <li className="grid grid-cols-3 justify-around mt-2" key={index}>
+                <div className="flex flex-row justify-around">{item.name}</div>
+                <div className="flex flex-row justify-around bg-gray-600 rounded-md">
+                  <button
+                    className="w-full text-white"
+                    onClick={() => {
+                      let newQuantity = item.quantity - 1;
+                      if (newQuantity > 0) {
+                        setQuantity(index, newQuantity);
+                      } else removeItemFromList(index);
+                    }}
+                  >
+                    -
+                  </button>
+                  <span className="bg-white w-full text-center">
+                    {item.quantity}
+                  </span>
+                  <button
+                    className="w-full text-white"
+                    onClick={() => {
+                      let newQuantity = item.quantity + 1;
+                      if (newQuantity > 0) {
+                        setQuantity(index, newQuantity);
+                      } else removeItemFromList(index);
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+                <div className="flex flex-row justify-around">
+                  ${item.price} - ${item.value.toFixed(2)}
+                </div>
+              </li>
+            ) : null}
+          </div>
+        ))}
+      </ul>
     </div>
   );
 };
